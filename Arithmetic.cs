92f0fad1449211f6a4c344d60557f43a9b1b5068
JamesToomer <jamesmtoomer@gmail.com>
2022-08-02 18:32:46 -0600

    First commit

---
+++ Arithmetic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Arithmetic
+{
+	static void Main()
+    {
+        int num1;
+        int num2;
+        double sum;
+
+        Console.Write("Enter first integer: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second integer: ");
+        num2 = int.Parse(Console.ReadLine());
+        sum = num1 + num2;
+
+        Console.WriteLine($"Sum is {sum}");
+        sum = num1 * num2;
+        Console.WriteLine($"Product is {sum}");
+        sum = num1 - num2;
+        Console.WriteLine($"Difference is {sum}");
+        sum = num1 / num2;
+        Console.WriteLine($"Quotient is {sum}");
+    }
+}
---
+++ TestRepo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            double sum;
+
+            Console.Write("Enter first integer: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second integer: ");
+            num2 = int.Parse(Console.ReadLine());
+            sum = num1 + num2;
+
+            Console.WriteLine($"Sum is {sum}");
+            sum = num1 * num2;
+            Console.WriteLine($"Product is {sum}");
+            sum = num1 - num2;
+            Console.WriteLine($"Difference is {sum}");
+            sum = num1 / num2;
+            Console.WriteLine($"Quotient is {sum}");
+        }
+
+    }
+}
