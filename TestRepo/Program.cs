4eabf5dcef169d2b9bf7e9ab9ad8e9b3a8e88732
JamesToomer <James.Toomer4478@omail.otech.edu>
2022-08-23 17:35:36 -0600

    Shapes

--- TestRepo/Program.cs
+++ TestRepo/Program.cs
@@ -48,7 +48,7 @@ namespace TestRepo
             Console.WriteLine($"Circumference is {2 * Math.PI * r}");
             Console.WriteLine($"Area is {Math.PI * (r * r)}");
         }
-        static void InvoiceAssignment()
+        /*static void InvoiceAssignment()
         {
             void Display(Invoice ToDisplay)
             {
@@ -61,7 +61,7 @@ namespace TestRepo
             }
 
             Display(new Invoice("AB001", "Test Item", 5, 125.23));
-        }
+        }*/
         static void comassignment()
         {
             Item i1 = new Item(1, (decimal)239.99);
@@ -71,46 +71,176 @@ namespace TestRepo
             Item[] Items = { i1, i2, i3, i4 };
             decimal TotalCom = 200;
             Console.WriteLine("Please Enter the number of items sold for each type: ");
-            foreach(Item i in Items)
+            foreach (Item i in Items)
             {
                 Console.Write($"Item {i.GetID()} (Commission Value: {i.GetCom()}): ");
                 TotalCom += int.Parse(Console.ReadLine()) * i.GetCom();
             }
-        
+
+        }
+        static void DupeAssignment()
+        {
+
+        }
+        static void LINQ()
+        {
+            Invoice[] invoices = new Invoice[8];
+            invoices[0] = new Invoice(83, "Electric sander", 7, 59.98m);
+            invoices[1] = new Invoice(24, "Power saw", 7, 99.99m);
+            invoices[2] = new Invoice(7, "Sledge hammer", 7, 21.50m);
+            invoices[3] = new Invoice(77, "Hammer", 7, 11.99m);
+            invoices[4] = new Invoice(39, "Lawn mower", 7, 79.50m);
+            invoices[5] = new Invoice(68, "Screwdriver", 7, 6.99m);
+            invoices[6] = new Invoice(56, "Jig saw", 7, 11.00m);
+            invoices[7] = new Invoice(3, "Wrench", 7, 7.50m);
+            var PartDesc =
+                from Invoice i in invoices
+                orderby i.PartDescription descending
+                select i;
+            var Price =
+                from Invoice i in invoices
+                orderby i.Price descending
+                select i;
+            var Quantity =
+                from Invoice i in invoices
+                orderby i.Quantity descending
+                select (i.PartDescription, i.Quantity);
+            var TotalCost =
+                from Invoice i in invoices
+                let InvoiceTotal = i.Price * i.Quantity
+                orderby InvoiceTotal descending
+                select new { i.PartDescription, InvoiceTotal };
+            var InRange =
+                from i in TotalCost
+                where (i.InvoiceTotal < 500 && i.InvoiceTotal > 200)
+                select i;
+            Console.WriteLine(InRange.GetType().ToString() + "\n\n\n");
+            Console.WriteLine("Original:");
+            foreach (var i in invoices) { Console.WriteLine($"{i}"); }
+            Console.WriteLine("\nSorted by description:");
+            foreach (var i in PartDesc) { Console.WriteLine($"{i}"); }
+            Console.WriteLine("\nSorted by price:");
+            foreach (var i in Price) { Console.WriteLine($"{i}"); }
+            Console.WriteLine("\nSorted by Quantity:");
+            foreach (var i in Quantity) { Console.WriteLine($"{i}"); }
+            Console.WriteLine("\nSorted by total cost of items:");
+            foreach (var i in TotalCost) { Console.WriteLine($"{i}"); }
+            Console.WriteLine("\nSorted by total cost of items:");
+            foreach (var i in TotalCost) { Console.WriteLine($"{i}"); }
+            Console.WriteLine("\nBetween $200 and $500:");
+            foreach (var i in InRange) { Console.WriteLine($"{i}"); }
+        }
+        static void States()
+        {
+            List<char> letters = new List<char>();
+            List<char> Distinct = new List<char>();
+
+            Random r = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                letters.Add((char)('A' + r.Next(0, 26)));
+            }
+            foreach (var c in letters.Distinct())
+            {
+                Distinct.Add(c);
+            }
+            var asc = from char c in letters orderby c ascending select c;
+            Console.WriteLine("Ascending: ");
+            foreach (char d in asc) { Console.Write(d); }
+            var desc = from char c in letters orderby c descending select c;
+            Console.WriteLine("\nDescending: ");
+            foreach (char d in desc) { Console.Write(d); }
+            var unique = from char c in Distinct orderby c ascending select c;
+
+            Console.WriteLine($"\nUnique (Ascending) TYPE = {unique.GetType().ToString()}: ");
+            foreach (char d in unique) { Console.Write(d); }
+            Console.WriteLine();
         }
         static void Main(string[] args)
         {
-            int asterisks = 1;
-            for(int i = 0; i < 4; i++)
+            Shape[] Shapes = { new Circle { Radius = 5 }, new Square { SideLength = 10 }, new Sphere { Radius = 7 }, new Cube { SideLength = 3 } };
+            int i = 1;
+            foreach(Shape s in Shapes)
             {
-                for (int k = 4-(asterisks/2); k > 0; k--)
-                {
-                    Console.Write(" ");
+                if(s is Shape2D) 
+                { 
+                    Console.Write($"{i} - {s.GetType()} (2D Shape): "); 
+                    if(s is Circle) { Console.WriteLine($"Radius is {(s as Circle).Radius} | Area is {(s as Circle).Area}"); }
+                    if (s is Square) { Console.WriteLine($"Side Length is {(s as Square).SideLength} | Area is {(s as Square).Area}"); }
                 }
-                for (int j = 0; j < asterisks; j++)
-                {
-                    Console.Write("*");
+                else if (s is Shape3D)
+                { 
+                    Console.Write($"{i} - {s.GetType()} (3D Shape) ");
+                    if (s is Sphere) { Console.WriteLine($"Radius is {(s as Sphere).Radius} | Area is {(s as Sphere).Area} | Volume is {(s as Sphere).Volume}"); }
+                    if (s is Cube) { Console.WriteLine($"Side Length is {(s as Cube).SideLength} | Area is {(s as Cube).Area} | Volume is {(s as Cube).Volume}"); }
                 }
-                
-                asterisks += 2;
-                Console.Write("\n");
             }
-            while(asterisks > 0)
-            {
-                for (int k = 4 - (asterisks/2); k > 0; k--)
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 0; j < asterisks; j++)
-                {
-                    Console.Write("*");
-                }
-                asterisks -= 2;
-                Console.Write("\n");
-            }
-        }
 
+        }
     }
+    public abstract class Shape { }
+    public abstract class Shape2D : Shape
+    {
+        public int a = 10;
+        public abstract double Area
+        {
+            get { return 0; }
+        }
+    }
+    public abstract class Shape3D : Shape 
+    {
+        public abstract double Area
+        {
+            get;
+        }
+        public abstract double Volume
+        {
+            get;
+        }
+    }
+    public class Circle : Shape2D
+    {
+        public double Radius;
+        public override double Area
+        {
+            get { return Math.PI*Math.Pow(Radius,2); }
+        }
+    }
+    public  class Square : Shape2D
+    {
+        public double SideLength;
+        public override double Area
+        {
+            get { return Math.Pow(SideLength,2); }
+        }
+    }
+    public  class Sphere : Shape3D
+    {
+        public double Radius;
+        public override double Area
+        {
+            get { return 4*Math.PI*Math.Pow(Radius,2); }
+        }
+        public override double Volume
+        {
+            get { return (4/3)*Math.PI*(Radius*3); }
+        }
+    }
+    public  class Cube : Shape3D
+    {
+        public double SideLength;
+        public override double Area
+        {
+            get { return 6*Math.Pow(SideLength,2); }
+        }
+        public override double Volume
+        {
+            get { return Math.Pow(SideLength,3); }
+        }
+    }
+
+
+
     public class Item
     {
         int _id;
@@ -129,14 +259,14 @@ namespace TestRepo
             return ((decimal)_value) * (decimal).09;
         }
     }
-    public class Invoice
+    public class oldInvoice
     { 
         //string Number{ get { return ""; } set{ Number = "Num: " } };
         string Number;
         string Description;
         int Quantity;
         double UnitPrice;
-        public Invoice(string PartNumber, string PartDescription, int PartQuantity, double price)
+        public oldInvoice(string PartNumber, string PartDescription, int PartQuantity, double price)
         {
             Number = PartNumber;
             Description = PartDescription;
@@ -156,5 +286,61 @@ namespace TestRepo
         public void SetDescription(string NewDescription) { Description = NewDescription; }
         public void SetUnitPrice(double NewPrice) { if (NewPrice > 0) { UnitPrice = NewPrice; } }
         public void SetQuantity(int NewQuantity) { if (NewQuantity > 0) { Quantity = NewQuantity; } }
+    }
+public class Invoice
+    {
+        // declare variables for Invoice object
+        private int quantityValue;
+        private decimal priceValue;
+        // auto-implemented property PartNumber
+        public int PartNumber { get; set; }
+        // auto-implemented property PartDescription
+        public string PartDescription { get; set; }
+        // four-argument constructor
+        public Invoice(int part, string description,
+           int count, decimal pricePerItem)
+        {
+            PartNumber = part;
+            PartDescription = description;
+            Quantity = count;
+            Price = pricePerItem;
+        }
+        // property for quantityValue; ensures value is positive
+        public int Quantity
+        {
+            get
+            {
+                return quantityValue;
+            }
+            set
+            {
+                if (value > 0) // determine whether quantity is positive
+                {
+                    quantityValue = value; // valid quantity assigned
+                }
+            }
+        }
+        // property for pricePerItemValue; ensures value is positive
+        public decimal Price
+        {
+            get
+            {
+                return priceValue;
+            }
+            set
+            {
+                if (value >= 0M) // determine whether price is non-negative
+                {
+                    priceValue = value; // valid price assigned
+                }
+            }
+        }
+        // return string containing the fields in the Invoice in a nice format
+        public override string ToString()
+        {
+            // left justify each field, and give large enough spaces so
+            // all the columns line up
+            return $"{PartNumber,-5} {PartDescription,-20} {Quantity,-5} {Price,6:C}";
+        }
     }
 }
